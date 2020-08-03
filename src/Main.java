91ef45a4385d5e97c87b967ea75489bfd75155d6
Jannatul Munera Jalen <jannatulmunera.jalen@students.mq.edu.au>
2020-08-04 09:36:48 +1000

    task 3
    done

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,24 @@ public class Main extends JFrame {
         public Canvas(){
             setPreferredSize(new Dimension(720,720));
         }
+
+         @Override
+         public void paint(Graphics g){
+            for(int i=10;i<710;i+=35){
+                for(int j=10;j<710;j+=35){
+                     g.setColor(Color.WHITE);
+                     g.fillRect(i, j, 35, 35);
+                     g.setColor(Color.BLACK);
+                     g.drawRect(i, j, 35, 35);
+                }
+            }
+         }
     }
+
     public Main(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas= new Canvas();
+        this.setContentPane(canvas);
         this.pack();
         this.setVisible(true);
     }
