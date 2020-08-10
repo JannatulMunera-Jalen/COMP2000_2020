2cb97072ab74282c296ed32e305b4a1fe26322a3
Jalen <jannatulmunera.jalen@student.mq.edu.au>
2020-08-11 00:27:54 +1000

    task 4
    eh~

---
+++ src/Cell.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+class Cell {
+    //fields
+    int x;
+    int y;
+
+    //constructors
+    public Cell(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+
+    //methods
+    void paint (Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, 35, 35);
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, 35, 35);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,17 @@ public class Main extends JFrame {
     }
 
     public class Canvas extends JPanel{
+
+        Grid grid;
+
         public Canvas(){
             setPreferredSize(new Dimension(720,720));
+            grid= new Grid();
         }
 
          @Override
          public void paint(Graphics g){
-            for(int i=10;i<710;i+=35){
-                for(int j=10;j<710;j+=35){
-                     g.setColor(Color.WHITE);
-                     g.fillRect(i, j, 35, 35);
-                     g.setColor(Color.BLACK);
-                     g.drawRect(i, j, 35, 35);
-                }
-            }
+             grid.paint(g);
          }
     }
 
